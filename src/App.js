90f90d331d5a85af488a90ee5bc967a61ec67f05
Mikhail326 <misha.stelmakh1995@gmail.com>
2021-12-23 13:16:10 +0300

    create__countries__app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Switch, Route, } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import { Details } from './pages/Details';
+import HomePage from './pages/HomePage';
+import { NotFound } from './pages/NotFound';
+
+
+function App() {
+  const [countries, setCountries] = useState([])
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <Switch>
+          <Route exact path='/'> <HomePage countries={countries} setCountries={setCountries}/> </Route>
+          <Route path='/country/:name' component={Details} /> 
+          <Route component={NotFound} /> 
+        </Switch>
+      </Main>
+    </>
+  );
+}
+
+export default App;
